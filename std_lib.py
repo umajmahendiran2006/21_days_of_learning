5a24b3af21df0f1cb88ce4614ccdcce66d491fe9
umajmahendiran2006 <umajmahendiran2006@gmail.com>
2026-02-23 06:39:14 +0530

    Add files via upload

---
+++ std_lib.py
@@ -0,0 +1,12 @@
+#random number
+import random
+print(random.randint(1,10))
+
+#random str
+import random
+print(random.choice(["apple","banana","orange","pineApple"]))
+
+#datetime
+import datetime
+print(datetime.datetime.now())
+print(datetime.date.today())
