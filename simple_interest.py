05cc8e38ab788780be22a44399cbd4ca1e8c44fa
umajmahendiran2006 <umajmahendiran2006@gmail.com>
2026-03-08 20:49:16 +0530

    Add files via upload

---
+++ simple_interest.py
@@ -0,0 +1,5 @@
+p=float(input("enter the principal amount: "))
+r=float(input("enter the rate of interest: "))
+t=float(input("enter the time in years: "))
+SI=(p*r*t)/100
+print("simple interest=",SI)
