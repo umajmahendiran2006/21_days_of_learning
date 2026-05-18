2af7121c79a7e1bd886c22b60a9091e9b911f29d
umajmahendiran2006 <umajmahendiran2006@gmail.com>
2026-05-18 20:29:19 +0530

    Add files via upload

---
+++ queue_operation.py
@@ -0,0 +1,6 @@
+que=[10,20,30]
+que.append(40) #enqueue
+print("queue after performing insert an element",que)
+que.pop()
+print("queue after performing delete an element",que)
+print("length of the queue",len(que))
